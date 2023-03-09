6250d75951684c06d2b7bf4990dd3e09d578c5fa
ulisesE <ulises.edu12@hotmail.com>
2023-03-08 21:46:09 -0600

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,7 @@
 <?php
 include 'header.php';
 session_start();
-if (!isset($_SESSION)) {
-    //header("location: login.html");
-    header("Location: http://www.example.com/something.php?a=1");
-}
+print_r($_SESSION);
 ?>
 <head>
     <title>Pixels - Admin</title>
