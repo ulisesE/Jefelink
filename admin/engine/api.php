5e7fa272460efc406a9287fd9195f3675d24970f
ulisesE <ulises.edu12@hotmail.com>
2023-03-10 12:15:16 -0600

    Update api.php

--- admin/engine/api.php
+++ admin/engine/api.php
@@ -2,6 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+session_start();
 include 'db.php';
 if (!isset($_GET['opc'])){
     $sql = "SELECT sum(precio) as suma,
