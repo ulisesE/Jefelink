340b28db87e84a4635b6ef53e313164cc0ac47f8
ulisesE <ulises.edu12@hotmail.com>
2023-03-10 19:30:49 -0600

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -262,9 +262,9 @@ include 'header.php';
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Dashboard - SB Admin</title>
+        <meta name="description" content="Pagina administrador de los negocios" />
+        <meta name="author" content="Ulises Reveles" />
+        <title><?= $_SESSION['auth']['nombreNegocio'];?> - Admin</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
