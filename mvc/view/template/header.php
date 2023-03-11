8134d9e15a34d30f9fc4b24bc7f0aaa1813caf3b
ulisesE <ulises.edu12@hotmail.com>
2023-03-10 20:48:03 -0600

    recursos bs

--- mvc/view/template/header.php
+++ mvc/view/template/header.php
@@ -7,7 +7,7 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title><?php echo $controller->page_title; ?></title>
-        <link href="css/styles.css" rel="stylesheet" />
+        <link href="src/css/styles.css" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
     </head>
 <!-- <!DOCTYPE html>
